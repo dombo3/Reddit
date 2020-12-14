2ce9408bf95bcfd768e832b3d4157fff8db0501d
Peter Dombovari <dombo.peter@gmail.com>
2020-12-14 17:25:06 +0100

    post Post

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 'use strict'
 
 const express = require('express');
+const Post = require('./models/Post');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const post = require('./models/Post');
 
-app.get('/', (req, res) => {
-  post.listAll((err, rows) => {
+app.use(express.json());
+
+app.get('/posts', (req, res) => {
+  post.listAll((err, result) => {
     if (err) {
-      return console.log(`Cannot get posts from db: ${err}`)
+      return console.log(`Cannot get posts from db: ${err}`);
     }
-    res.send(rows);
+    res.send(result);
   });
-})
+});
+
+app.post('/posts', (req, res) => {
+  if (req.body.title && req.body.url) {
+    const newPost = new Post(req.body);
+    post.addPost(newPost, (err, result) => {
+      if (err) {
+        return console.log(`Cannot insert post to db: ${err}`);
+      }
+      res.send(result);
+    });
+  } else {
+    res.send("Please provide title and url in the post");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port: ${PORT}`);
-})
+});
--- models/Post.js
+++ models/Post.js
@@ -2,18 +2,37 @@
 
 const db = require('../db');
 
-let Post = function () {
+let Post = function(post) {
+  this.title = post.title;
+  this.url = post.url;
 }
 
-Post.listAll = function (result) {
+Post.listAll = function(result) {
   const query = 'SELECT * FROM post';
-  db.query(query, (err, rows) => {
+  db.query(query, (err, posts) => {
     if (err) {
       console.error(`Cannot retrive data from db ${err.toString()}`);
       return result(err, null);
     }
-    result(null, rows);
+    result(null, posts);
   })
 }
+
+Post.addPost = function(post, res) {
+  const isoTimestamp = new Date().toISOString();
+  const timestamp = isoTimestamp.replace('T', " ").replace(/\..*/, "");
+  const score = 0;
+  const query = `INSERT INTO post (title, url, timestamp, score) VALUES ("?", "?", "${timestamp}", ${score})`;
+  db.query(query, [post.title, post.url], (err, result, fields) => {
+    if (err) {
+      console.error(`Cannot insert data to db ${err.toString()}`);
+      return res(err, null);
+    }
+    post['id'] = result.insertId;
+    post['timestamp'] = timestamp;
+    post['score'] = score;
+    res(null, post);
+  });
+};
 
 module.exports = Post;
